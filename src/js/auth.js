2189c979afced28d6622807a488ca5009a17e923
yungsamd17 <yungsamd@proton.me>
2023-11-20 23:16:32 +0100

    🔧Potentional redirectURL fix

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,5 +1,5 @@
 const TWITCH_APP_TOKEN = "veho7ytn25l8a9dpgfkk79sqgey43j";
-const redirectURL = chrome.identity.getRedirectURL();
+const redirectURL = "https://yungsamd17.github.io/Twitch-Live/twitch-auth-redirect/";
 
 const handleTwitchUnauthorized = () => {
   chrome.storage.local.set({
