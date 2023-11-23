2c648262f2aa41fe2aa6d06b08c2a5c7fca56434
yungsamd17 <yungsamd@proton.me>
2023-11-23 16:27:01 +0100

    🔧Update: uptime timer format

--- src/js/util.js
+++ src/js/util.js
@@ -1,16 +1,14 @@
 const getTimePassed = (startTime) => {
   let elapsedTime = Date.now() - Date.parse(startTime);
-  elapsedTime = (elapsedTime - (elapsedTime % 1000)) / 1000;
-  const ss = Math.floor(elapsedTime % 60);
-  elapsedTime = (elapsedTime - ss) / 60;
-  const mm = Math.floor(elapsedTime % 60);
-  elapsedTime = (elapsedTime - mm) / 60;
-  const hh = Math.floor(elapsedTime % 24);
+  const hours = Math.floor(elapsedTime / 3600000);
+  const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
 
-  const format = (s) => (s < 10 ? `0${s}` : s);
-  
-  // Only include hours if they are greater than zero
-  const hoursPart = hh > 0 ? `${format(hh)}:` : '';
+  const format = (s) => (s < 10 && s !== 0 ? `0${s}` : s);
 
-  return `${hoursPart}${format(mm)}:${format(ss)}`;
+  const formattedHours = hours > 0 ? `${hours}:` : '';
+  const formattedMinutes = hours > 0 || minutes >= 10 ? format(minutes) : minutes;
+  const formattedSeconds = `${format(seconds)}`;
+
+  return `${formattedHours}${formattedMinutes}:${formattedSeconds}`;
 };
