f53ab283939efed60bf66b6b39470e83587329f7
yungsamd17 <yungsamd@proton.me>
2023-11-25 14:25:52 +0100

    🔧Update util.js timer seconds

--- src/js/util.js
+++ src/js/util.js
@@ -4,7 +4,7 @@ const getTimePassed = (startTime) => {
   const minutes = Math.floor((elapsedTime % 3600000) / 60000);
   const seconds = Math.floor((elapsedTime % 60000) / 1000);
 
-  const format = (s) => (s < 10 && s !== 0 ? `0${s}` : s);
+  const format = (s) => (s < 10 ? `0${s}` : s);
 
   const formattedHours = hours > 0 ? `${hours}:` : '';
   const formattedMinutes = hours > 0 || minutes >= 10 ? format(minutes) : minutes;
