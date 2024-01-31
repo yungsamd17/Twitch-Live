b2db8afb1ba7ede4ee77f71c05a6f9dd2379d2ba
yungsamd17 <yungsamd@proton.me>
2024-01-31 15:11:54 +0100

    🔧Fix typo with pressing `f` to close settings modal

--- src/js/settings.js
+++ src/js/settings.js
@@ -250,10 +250,9 @@ function closeModal() {
 settingsCloseBtn.onclick = closeModal;
 // Close modal when the 'f' key is pressed, but not in an input field
 document.addEventListener('keydown', function(event) {
-    // Check if the 'f' key is pressed
     if (event.key.toLowerCase() === 'f') {
         // Check if the event target is an input element, and if not close the modal
-        if (document.activeElement.tagName.toLowerCase() !== 'input') {s
+        if (document.activeElement.tagName.toLowerCase() !== 'input') {
             closeModal();
         }
     }
